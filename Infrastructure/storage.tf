8ecbc7098174f879adfd58a8d1ea147e5835c70d
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-03 09:27:28 +0000

    Storage cambiado

--- Infrastructure/storage.tf
+++ Infrastructure/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sa_aut" {
-  name                     = "autstorageaccount"
+  name                     = "minaya_storageaccount"
   resource_group_name      = azurerm_resource_group.rg_aut.name
   location                 = azurerm_resource_group.rg_aut.location
   account_tier             = "Standard"
