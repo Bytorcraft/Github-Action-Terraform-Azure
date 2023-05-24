5fde955c8bc23efce983de43fb46f1be98d3db91
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-24 14:47:09 +0000

    Nueva vm

--- Infrastructure/vm.tf
+++ Infrastructure/vm.tf
@@ -38,7 +38,7 @@ resource "azurerm_linux_virtual_machine" "aut_vm" {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
   }
-  
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
