419def010e04b8154b45642e999f028d6f7f4117
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-24 14:35:43 +0000

    Nueva vm

--- Infrastructure/vm.tf
+++ Infrastructure/vm.tf
@@ -2,14 +2,14 @@
 resource "azurerm_virtual_network" "aut_vnet" {
   name                = "aut_vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg_aut.name
-  resource_group_name = azurerm_resource_group.rg_aut.location
+  location            = azurerm_resource_group.rg_aut.location
+  resource_group_name = azurerm_resource_group.rg_aut.name
 }
 
 resource "azurerm_subnet" "aut_subnet" {
   name                 = "aut_subnet"
   resource_group_name  = azurerm_resource_group.rg_aut.name
-  virtual_network_name = azurerm_resource_group.rg_aut.location
+  virtual_network_name = "azurerm_virtual_network.aut_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
