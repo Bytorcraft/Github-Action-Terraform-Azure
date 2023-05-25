7deda5afad30f9efcceab0661a9c794c3bbd25ad
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-25 08:46:47 +0000

    Nuevo KeyVault

--- Infrastructure/keyvault.tf
+++ Infrastructure/keyvault.tf
@@ -27,6 +27,6 @@ resource "azurerm_key_vault_key" "aut_kvk" {
   key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
 
   depends_on = [ 
-    azurerm_key_vault_access_policy.client
+    azurerm_key_vault_access_policy.client,
   ]
 }
