22f512e2c73965bfa9fbbc0b8d16536ee63f266e
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-25 08:44:35 +0000

    Nuevo KeyVault

--- Infrastructure/keyvault.tf
+++ Infrastructure/keyvault.tf
@@ -10,10 +10,23 @@ resource "azurerm_key_vault" "aut_kv" {
   purge_protection_enabled = true
 }
 
+resource "azurerm_key_vault_access_policy" "client" {
+  key_vault_id = azurerm_key_vault.aut_kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "13e17103-d412-48b8-89bb-cc78af118ae6"
+
+  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify", "GetRotationPolicy"]
+  secret_permissions = ["Get", "Set", "List", "Delete"]
+}
+
 resource "azurerm_key_vault_key" "aut_kvk" {
   name         = "auto-storage-key"
   key_vault_id = azurerm_key_vault.aut_kv.id
   key_type     = "RSA"
   key_size     = 2048
   key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+
+  depends_on = [ 
+    azurerm_key_vault_access_policy.client
+   ]
 }
