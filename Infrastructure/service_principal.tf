68b87cb2173b6f573e15dc3eb0054b30b71b3e9b
Bytorcraft <93713209+Bytorcraft@users.noreply.github.com>
2023-05-25 09:15:08 +0000

    Nuevos Recursos

--- Infrastructure/service_principal.tf
+++ Infrastructure/service_principal.tf
@@ -1,12 +1,10 @@
 resource "azuread_application" "terraform" {
   display_name = "terraform"
-  owner        = "13e17103-d412-48b8-89bb-cc78af118ae6"
 }
 
 resource "azuread_service_principal" "terraform" {
   application_id               = azuread_application.terraform.application_id
   app_role_assignment_required = false
-  owner                        = "13e17103-d412-48b8-89bb-cc78af118ae6"
 }
 
 resource "azuread_service_principal_password" "terraform" {
